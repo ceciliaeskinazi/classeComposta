a5a46edd829619fab1a7929ac91dd7c0d24e934b
ceciliaeskinazi <168425472+ceciliaeskinazi@users.noreply.github.com>
2024-06-05 07:52:31 -0300

    Create CachorroMain.java

---
+++ CachorroMain.java
@@ -0,0 +1,8 @@
+public class CachorroMain {
+
+	public static void main(String[] args) {
+		Cachorro ref = new Cachorro("Eskilate", "Labrador", 11, new Data(11, 4, 2014));
+			System.out.println(ref.toString());
+	}
+
+}
